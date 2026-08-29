44a905d019aa7ef65a91c0d5501caacd51ee431a
Ayomide <khumzerauchiha@gmail.com>
2026-08-29 23:35:22 +0100

    JS cleaning up

---
+++ scripts/newcoinflip.js
@@ -0,0 +1,8 @@
+   comp_pick = Math.random();
+      comp_ans = ''
+      if(comp_pick < 1/3){
+        comp_ans = 'Heads'
+      }
+      else if (comp_pick > 1/3 && comp_pick <2/3){
+        comp_ans = 'Tails'
+      }
