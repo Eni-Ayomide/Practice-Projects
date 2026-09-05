c25b916d6193e4777eb7f0812b004d35ca03af7e
Ayomide <khumzerauchiha@gmail.com>
2026-09-05 01:01:31 +0100

    Todo List With Js

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,6 +1,4 @@
 const todolist = [
-    "wash clothes",
-    " make dinner"
 ];
 
 rendertodolist()
@@ -9,11 +7,22 @@ function rendertodolist(){
     let todolistHTML = '';
 
     for(let i = 0; i < todolist.length; i++){
-      const todo = todolist[i];
-      const html = `<p>${todo}</p>`;
+      const todoObject = todolist[i];
+      //const name = todoObject.name;
+      const {name, duedate}=todoObject;
+      //const duedate = todoObject.duedate;
+      const html = `
+      <div>${name}</div>
+      <div>${duedate}</div>
+      <button class = "delete-todo-button" onclick="
+
+      todolist.splice(${i}, 1)
+      rendertodolist()
+      
+      ">Delete</button>
+      `;
       todolistHTML += html;
     }
-    console.log(todolistHTML);
 
     document.querySelector('.js-todo-list').innerHTML
       = todolistHTML
@@ -21,12 +30,18 @@ function rendertodolist(){
 }
 function addtodo(){
   const inputvalue = document.querySelector('.js-name-input');
+  const name = inputvalue.value;
 
-  const todoname = inputvalue.value;
+  const dateinput = document.querySelector('.js-duedate')
 
-  todolist.push(todoname);
-
-  console.log(todolist)
+  const duedate=dateinput.value;
+  
+  todolist.push({
+  //  name: todoname,
+  //duedate: dueDate
+    name, 
+    duedate
+  });
 
   
 
